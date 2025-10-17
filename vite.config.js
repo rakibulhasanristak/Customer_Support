1be05871bf1f1beec3251aabf423306b4e00c4ef
rakibulhasanristak <rakibulhasanristak@gmail.com>
2025-10-17 21:17:30 +0600

    chore: add Netlify config and set base for Customer_Support

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
    
   plugins: [react(), tailwindcss()],
-  base: "/customer-Support/",
+  base: "/Customer_Support/",
 });
